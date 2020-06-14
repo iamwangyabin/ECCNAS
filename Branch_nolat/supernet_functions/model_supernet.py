26e5528e8acef7e2657af92614095326123c1c92
王亚斌 <38123329+iamwangyabin@users.noreply.github.com>
2020-06-14 18:23:47 +0800

    add new

---
+++ Branch_nolat/supernet_functions/model_supernet.py
@@ -0,0 +1,109 @@
+import torch
+from torch import nn
+from fbnet_building_blocks.fbnet_builder import ConvBNRelu, Flatten
+from supernet_functions.config_for_supernet import CONFIG_SUPERNET
+from supernet_functions.loss import Post_Prob, Bay_Loss
+import torch.utils.checkpoint as checkpoint
+
+
+class MixedOperation(nn.Module):
+    # Arguments:
+    # proposed_operations is a dictionary {operation_name : op_constructor}
+    def __init__(self, layer_parameters, proposed_operations, latency):
+        super(MixedOperation, self).__init__()
+        ops_names = [op_name for op_name in proposed_operations]
+        self.ops = nn.ModuleList([proposed_operations[op_name](*layer_parameters)
+                                  for op_name in ops_names])
+        self.thetas = nn.Parameter(torch.Tensor([1.0 / len(ops_names) for i in range(len(ops_names))]))
+    
+    def forward(self, x, temperature, latency_to_accumulate):
+        soft_mask_variables = nn.functional.gumbel_softmax(self.thetas, temperature)
+        output  = sum(m * op(x) for m, op in zip(soft_mask_variables, self.ops))
+        return output, latency_to_accumulate
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+cfg = {
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512]
+}
+
+class FBNet_Stochastic_SuperNet(nn.Module):
+    def __init__(self, branch1, branch2, branch3):
+        super(FBNet_Stochastic_SuperNet, self).__init__()
+        # self.first = ConvBNRelu(input_depth=3, output_depth=16, kernel=3, stride=1,
+        #                         pad=1, no_bias=1, use_relu="relu", bn_type="bn")
+        # self.branch1_to_search = nn.ModuleList([MixedOperation(
+        #                                            branch1.layers_parameters[layer_id],
+        #                                            branch1.lookup_table_operations,
+        #                                            branch1.lookup_table_latency[layer_id])
+        #                                        for layer_id in range(branch1.cnt_layers)])
+        # self.branch2_to_search = nn.ModuleList([MixedOperation(
+        #                                            branch2.layers_parameters[layer_id],
+        #                                            branch2.lookup_table_operations,
+        #                                            branch2.lookup_table_latency[layer_id])
+        #                                        for layer_id in range(branch2.cnt_layers)])
+        # self.branch3_to_search = nn.ModuleList([MixedOperation(
+        #                                            branch3.layers_parameters[layer_id],
+        #                                            branch3.lookup_table_operations,
+        #                                            branch3.lookup_table_latency[layer_id])
+        #                                        for layer_id in range(branch3.cnt_layers)])
+        # self.last_stages = nn.Sequential(
+        #     ConvBNRelu(input_depth=512, output_depth=64, kernel=3, stride=1,
+        #                pad=1, no_bias=1, use_relu="relu", bn_type="bn"),
+        #     nn.Conv2d(64,1,1)
+        # )
+        self.features = make_layers(cfg['E'])
+        self.reg_layer = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 1, 1)
+        )
+    # def custom(self, module):
+    #     def custom_forward(*inputs):
+    #         output = module(inputs[0][0], inputs[0][1], inputs[0][2])
+    #         return output
+    #     return custom_forward
+    def forward(self, x, temperature):
+        # y = self.first(x)
+        # for mixed_op in self.branch1_to_search:
+        #     y1, latency_to_accumulate = checkpoint.checkpoint(self.custom(mixed_op),[y1, temperature, latency_to_accumulate])
+        # for mixed_op in self.branch2_to_search:
+        #     y2, latency_to_accumulate = mixed_op(y2, temperature, latency_to_accumulate)
+        #     y2, latency_to_accumulate = checkpoint.checkpoint(self.custom(mixed_op),[y2, temperature, latency_to_accumulate])
+        # for mixed_op in self.branch3_to_search:
+        #     y = mixed_op(y, temperature)
+        y = self.features(x)
+        # y1 = torch.nn.Upsample(scale_factor=0.125, mode="bilinear")(y1)
+        y = torch.nn.Upsample(scale_factor=2, mode="bilinear")(y)
+        # y = torch.cat([y2,y3], dim=1)
+        # y = self.last_stages(y)
+        y = self.reg_layer(y)
+        return torch.abs(y)
+
+class SupernetLoss(nn.Module):
+    def __init__(self, sigma, crop_size, downsample_ratio, background_ratio, use_background):
+        super(SupernetLoss, self).__init__()
+        self.post_prob = Post_Prob(sigma, crop_size, downsample_ratio, background_ratio, use_background)
+        self.criterion = Bay_Loss(use_background)
+        self.beta = CONFIG_SUPERNET["loss"]['beta']
+        self.alpha = CONFIG_SUPERNET["loss"]['alpha']
+
+    def forward(self, pred, points, target, st_sizes):
+        prob_list = self.post_prob(points, st_sizes)
+        ce = self.criterion(prob_list, target, pred)
+        return ce
---
+++ Branch_nolat/supernet_functions/training_functions_supernet.py
@@ -0,0 +1,110 @@
+import torch
+import time
+from general_functions.utils import AverageMeter, save
+from supernet_functions.config_for_supernet import CONFIG_SUPERNET
+import numpy as np
+
+class TrainerSupernet:
+    def __init__(self, criterion, w_optimizer, theta_optimizer, w_scheduler, logger, writer):
+        self.logger = logger
+        self.writer = writer
+        
+        self.criterion = criterion
+        self.w_optimizer = w_optimizer
+        self.theta_optimizer = theta_optimizer
+        self.w_scheduler = w_scheduler
+        
+        self.temperature                 = CONFIG_SUPERNET['train_settings']['init_temperature']
+        self.exp_anneal_rate             = CONFIG_SUPERNET['train_settings']['exp_anneal_rate'] # apply it every epoch
+        self.cnt_epochs                  = CONFIG_SUPERNET['train_settings']['cnt_epochs']
+        self.train_thetas_from_the_epoch = CONFIG_SUPERNET['train_settings']['train_thetas_from_the_epoch']
+        self.print_freq                  = CONFIG_SUPERNET['train_settings']['print_freq']
+        self.path_to_save_model          = CONFIG_SUPERNET['train_settings']['path_to_save_model']
+        self.best_mse = 99999
+        self.best_mae = 99999
+        self.device = "cuda"
+
+    def train_loop(self, train_w_loader, train_thetas_loader, test_loader, model):
+        # firstly, train weights only
+        for epoch in range(self.train_thetas_from_the_epoch):
+            self.writer.add_scalar('learning_rate/weights', self.w_optimizer.param_groups[0]['lr'], epoch)
+            self.logger.info("Firstly, start to train weights for epoch %d" % (epoch))
+            self._training_step(model, train_w_loader, self.w_optimizer, epoch, info_for_logger="_w_step_")
+            self._validate(model, test_loader, epoch)
+          
+        for epoch in range(self.train_thetas_from_the_epoch, self.cnt_epochs):
+            self.writer.add_scalar('learning_rate/weights', self.w_optimizer.param_groups[0]['lr'], epoch)
+            # self.writer.add_scalar('learning_rate/theta', self.theta_optimizer.param_groups[0]['lr'], epoch)
+            
+            self.logger.info("Start to train weights for epoch %d" % (epoch))
+            self._training_step(model, train_w_loader, self.w_optimizer, epoch, info_for_logger="_w_step_")
+
+            self.logger.info("Start to train theta for epoch %d" % (epoch))
+            self._training_step(model, train_thetas_loader, self.theta_optimizer, epoch, info_for_logger="_theta_step_")
+
+            self.temperature = self.temperature * self.exp_anneal_rate
+
+    def _training_step(self, model, loader, optimizer, epoch, info_for_logger=""):
+        epoch_loss = AverageMeter()
+        epoch_mae = AverageMeter()
+        epoch_mse = AverageMeter()
+        start_time = time.time()
+
+        model = model.train()
+        epoch_length = len(loader)
+        for step, (inputs, points, targets, st_sizes) in enumerate(loader):
+            inputs = inputs.to(self.device)
+            st_sizes = st_sizes.to(self.device)
+            gd_count = np.array([len(p) for p in points], dtype=np.float32)
+            points = [p.to(self.device) for p in points]
+            targets = [t.to(self.device) for t in targets]
+            if info_for_logger == "_theta_step_" and step > epoch_length/3:
+                continue
+            with torch.set_grad_enabled(True):
+                outs = model(inputs, self.temperature)
+                loss = self.criterion(outs, points, targets, st_sizes)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                N = inputs.shape[0]
+                pre_count = torch.sum(outs.view(N, -1), dim=1).detach().cpu().numpy()
+                res = pre_count - gd_count
+                epoch_loss.update(loss.item(), N)
+                epoch_mse.update(np.mean(res * res), N)
+                epoch_mae.update(np.mean(abs(res)), N)
+            if step % self.print_freq ==0:
+                self.logger.info('Epoch {} Step {} Train, Loss: {:.2f}/({:.2f}), MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
+                         .format(epoch, step, epoch_loss.get_avg(),loss.item(), np.sqrt(epoch_mse.get_avg()), epoch_mae.get_avg(), time.time()-start_time))
+
+        self.writer.add_scalar('train_vs_val/' + "train" + '_loss' + info_for_logger, epoch_loss.get_avg(), epoch)
+        self.writer.add_scalar('train_vs_val/' + "train" + '_mse' + info_for_logger, np.sqrt(epoch_mse.get_avg()), epoch)
+        self.writer.add_scalar('train_vs_val/' + "train" + '_mae' + info_for_logger, epoch_mae.get_avg(), epoch)
+
+    def _validate(self, model, loader, epoch):
+        model.eval()
+        start_time = time.time()
+        epoch_res = []
+        with torch.no_grad():
+            for step, (inputs, count, name) in enumerate(loader):
+                inputs = inputs.cuda()
+                # inputs are images with different sizes
+                assert inputs.size(0) == 1, 'the batch size should equal to 1 in validation mode'
+                with torch.set_grad_enabled(False):
+                    outs = model(inputs, self.temperature)
+                    res = count[0].item() - torch.sum(outs).item()
+                    epoch_res.append(res)
+        epoch_res = np.array(epoch_res)
+        mse = np.sqrt(np.mean(np.square(epoch_res)))
+        mae = np.mean(np.abs(epoch_res))
+        self.logger.info('Epoch {} Val, MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
+                     .format(epoch, mse, mae, time.time()-start_time))
+        self.writer.add_scalar('train_vs_val/' + "val" + '_mse' + '', mse, epoch)
+        self.writer.add_scalar('train_vs_val/' + "val" + '_mae' + '', mae, epoch)
+
+        if (2.0 * mse + mae) < (2.0 * self.best_mse + self.best_mae):
+            self.best_mse = mse
+            self.best_mae = mae
+            self.logger.info("save best mse {:.2f} mae {:.2f} model epoch {}".format(self.best_mse,
+                                                                                 self.best_mae,
+                                                                                 epoch))
+            torch.save(model.state_dict(), self.path_to_save_model)
